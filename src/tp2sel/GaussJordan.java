109d94394cd9ca2791ab299908a48f73177dbe68
pdiego93 <pdiego93@gmail.com>
2015-09-09 11:22:35 -0300

    GJ casi listo. Creo xD

--- src/tp2sel/GaussJordan.java
+++ src/tp2sel/GaussJordan.java
@@ -2,8 +2,9 @@ package tp2sel;
 
 
 public class GaussJordan {
-
-	public static boolean esIdentidad(int m1[][])
+	
+	//Tiene sysos por todos lados porque quería ver paso a paso que hacía para detectar los errores
+	public static boolean esIdentidad(double m1[][])
 	{
 		for (int i=0;i<5;i++)
 			for(int j=0;j<5;j++)
@@ -15,34 +16,75 @@ public class GaussJordan {
 			}
 		return true;
 	}
-	public static int gaussJordan(int m1[][], int m2[][]){
-		int k, i=0, y=0, j=0, a=0;
-		while(!esIdentidad(m1)){
-			if(i>=5)
+	public static int gaussJordan(double m1[][], double m2[][]){
+		int i=0, y=0, j=0, a=0, z=0;
+		double k;
+		while(!esIdentidad(m1) && z<10){
+			System.out.println(i);
+			if(i==5)
 				i=0;
+			
 			for(y=0;y<5;y++){
-				if(m1[i][i]!=0){
+				if(m1[y][i]!=0){
 				
-					k=1/m1[i][i];
-				
+					k = 1/m1[y][i];
+					//System.out.println("Prueba"+ y);
 				for(j=0;j<5;j++)
 				{
 					m1[y][j]*=k;
 					m2[y][j]*=k;
-					
+					//System.out.println("Prueba"+j);
+					for(int l=0;l<5;l++)
+					{	for(int h=0; h<5; h++)
+							{	System.out.print(m1[l][h]);
+								
+							}
+						System.out.println("");
+					}
+					System.out.println("---------");
 				}
 				}
 			}
 			for(y=0; y<5; y++)
 			{
-				if(i!=y)
+				if(i!=y){
+					if(y<i)
+						k = m1[y][i];
+					else
+						k=1;
 					for(j=0;j<5;j++){
-						m1[y][j]-=m1[i][j];
-						m2[y][j]-=m2[i][j];
+					
+					
+						m1[y][j]-=m1[i][j]*k;
+						m2[y][j]-=m2[i][j]*k;
+						//System.out.println("Prueba"+j+i);
+						for(int l=0;l<5;l++)
+						{	for(int h=0; h<5; h++)
+								{	System.out.print(m1[l][h]);
+									
+								}
+							System.out.println("");
+						}
+						System.out.println("---------");
+					}
 					}
 			}
+			System.out.println("Prueba"+i +","+z);
+			z++;
+			for(y=0;y<5;y++)
+			{	for(j=0; j<5; j++)
+					{	System.out.print(m1[y][j]);
+						
+					}
+				System.out.println("");
+			}
+			System.out.println("---------");
+			
 			
 			i++;
+			
+			
+			
 		}
 		a=1;
 		return a;
@@ -51,8 +93,8 @@ public class GaussJordan {
 	
 	
 	public static void main(String[] args) {
-		int [][] m = new int[5][5];
-		int [][] n = new int[5][5];
+		double [][] m = new double[5][5];
+		double [][] n = new double[5][5];
 		
 		for(int i=0;i<5;i++)
 			for(int j=0; j<5; j++)
@@ -64,7 +106,13 @@ public class GaussJordan {
 					if(i==j)n[i][j]= 1;
 					else n[i][j]= 0;
 				}
-		
+		for(int i=0;i<5;i++)
+		{	for(int j=0; j<5; j++)
+				{	System.out.print(m[i][j]);
+					
+				}
+			System.out.println("");
+		}
 		for(int i=0;i<5;i++)
 		{	for(int j=0; j<5; j++)
 				{	System.out.print(n[i][j]);
@@ -75,7 +123,7 @@ public class GaussJordan {
 		System.out.println("Prueba");
 		
 		System.out.println(gaussJordan(m,n));
-		System.out.println("slsls");
+		
 		for(int i=0;i<5;i++)
 		{	for(int j=0; j<5; j++)
 				{	System.out.print(m[i][j]);
